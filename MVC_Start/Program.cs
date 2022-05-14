3e91e964397a7a431e12897cb82255b38658cb4b
Raquel Lee <Raquel_Lee@outlook.com>
2022-05-14 14:28:28 -0400

    raquel

--- MVC_Start/Program.cs
+++ MVC_Start/Program.cs
@@ -20,6 +20,6 @@ namespace MVC_Start
     public static IWebHost BuildWebHost(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
-            .Build();
+            .Build(); //Raquel was here
   }
 }
